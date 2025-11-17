bad9f8a4d9498b98f2f3115ae48a5e10661636a1
Roman <hpp1ovh@gmail.com>
2025-11-17 16:09:33 +0200

    task fixs

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
 import { Tabs } from './components/Tabs/Tabs';
 
+// Sample tabs data
+
 export const tabs = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
